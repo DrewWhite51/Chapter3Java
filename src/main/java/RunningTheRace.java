e702e3508748f6a7fd9c7650a91ef98d0a1eb0a1
Drew <drew.m.white51@gmail.com>
2020-12-18 15:18:32 -0600

    Updates.

--- src/main/java/RunningTheRace.java
+++ src/main/java/RunningTheRace.java
@@ -12,48 +12,46 @@ import java.util.Scanner;
  * @author Drew
  */
 public class RunningTheRace {
-    
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        
-
-
-        // Gathering input from user
-        System.out.println("Enter the name of the first runner");
-        String firstRunner = keyboard.nextLine();    
-        System.out.println("Enter the name of the second runner");
-        String secondRunner = keyboard.nextLine();      
-        System.out.println("Enter the name of the third runner");
+        System.out.println("Enter the name of the first runner.");
+        String firstRunner = keyboard.nextLine();
+        System.out.println("Enter the name of the second runner.");
+        String secondRunner = keyboard.nextLine();
+        System.out.println("Enter the name of the third runner.");
         String thirdRunner = keyboard.nextLine();
         
+        System.out.println("Enter the time it took for the first runner to finish the race in minutes");
+        int firstRunnerTime = keyboard.nextInt();
+        System.out.println("Enter the time it took for the second runner to finish the race in minutes");
+        int secondRunnerTime = keyboard.nextInt();
+        System.out.println("Enter the time it took for the third runner to finish the race in minutes");
+        int thirdRunnerTime = keyboard.nextInt();
         
-        
-        
-        System.out.println("Enter the time of the first runner in minutes");
-        double firstRunnerTime = keyboard.nextDouble();
-        System.out.println("Enter the time of the second runner in minutes");
-        double secondRunnerTime = keyboard.nextDouble();
-        System.out.println("Enter the time of the third runner in minutes");
-        double thirdRunnerTime = keyboard.nextDouble();
-        
-        
-        // Ouput
-        
-        if ((firstRunnerTime > secondRunnerTime) & (firstRunnerTime > thirdRunnerTime)) {
-            System.out.println(firstRunner + " finihsed in first place.");
-            if (secondRunnerTime > thirdRunnerTime) {
-            System.out.println(secondRunner + " finihsed in second place.");
-            if (secondRunnerTime < thirdRunnerTime) {
-            System.out.println(secondRunner + " finihsed in second place.");
-                    }
-            
-        } else if ((secondRunnerTime > firstRunnerTime) & (secondRunnerTime > thirdRunnerTime)) {
-            System.out.println(secondRunner + " finished in first.");
-        } else if ((thirdRunnerTime > firstRunnerTime) & (thirdRunnerTime > secondRunnerTime)) {
-            System.out.println(thirdRunner + " finished in first.");
+        if (firstRunnerTime < secondRunnerTime && secondRunnerTime < thirdRunnerTime){
+            System.out.println(firstRunner + " came in first");
+            System.out.println(secondRunner + " came in second");
+            System.out.println(thirdRunner + " came in third");
+        } else if (firstRunnerTime < thirdRunnerTime && thirdRunnerTime < secondRunnerTime) {
+            System.out.println(firstRunner + " came in first");
+            System.out.println(thirdRunner + " came in second");
+            System.out.println(secondRunner + " came in third");
+        } else if (secondRunnerTime < firstRunnerTime && firstRunnerTime < thirdRunnerTime) {
+            System.out.println(secondRunner + " came in first");
+            System.out.println(firstRunner + " came in second");
+            System.out.println(thirdRunner + " came in third");
+        } else if (secondRunnerTime < thirdRunnerTime && thirdRunnerTime < firstRunnerTime){
+            System.out.println(secondRunner + " came in first");
+            System.out.println(thirdRunner + " came in second");
+            System.out.println(firstRunner + " came in third");
+        } else if (thirdRunnerTime < secondRunnerTime && secondRunnerTime < firstRunnerTime) {
+            System.out.println(thirdRunner + " came in first");
+            System.out.println(secondRunner + " came in second");
+            System.out.println(firstRunner + " came in third");
+        } else if (thirdRunnerTime < firstRunnerTime && firstRunnerTime < secondRunnerTime) {
+            System.out.println(thirdRunner + " came in first");
+            System.out.println(firstRunner + " came in second");
+            System.out.println(secondRunner + " came in third");
         }
-        
-        
     }
-    
 }
